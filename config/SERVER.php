ee1a19c68c60f1cad9a499a2d9e36fac425feab9
Yerridev <cramirezabondan@uss.edu.pe>
2025-03-14 21:29:41 -0500

    variable entorno

--- config/SERVER.php
+++ config/SERVER.php
@@ -16,15 +16,31 @@
 	// const SECRET_IV='102791';
 	
 	/*----------  Datos del servidor  ----------*/
-	define("SERVER", getenv("DB_HOST") ?: "localhost");
-	define("DB", getenv("DB_DATABASE") ?: "ser_mecanica");
-	define("USER", getenv("DB_USERNAME") ?: "root");
-	define("PASS", getenv("DB_PASSWORD") ?: "");
+	// define("SERVER", getenv("DB_HOST") ?: "localhost");
+	// define("DB", getenv("DB_DATABASE") ?: "ser_mecanica");
+	// define("USER", getenv("DB_USERNAME") ?: "root");
+	// define("PASS", getenv("DB_PASSWORD") ?: "");
+
+	// define("SGBD", "mysql:host=".SERVER.";dbname=".DB);
+
+	// /*----------  Datos de la encriptación  ----------*/
+	// define("METHOD", getenv("METHOD") ?: "AES-256-CBC");
+	// define("SECRET_KEY", getenv("SECRET_KEY") ?: '$STO@2021');
+	// define("SECRET_IV", getenv("SECRET_IV") ?: '102791');
+
+	// mysql://root:pgEQaMsjAbuWpwuboHqMSxXielvbqnnx@switchback.proxy.rlwy.net:29558/railway
+
+
+	/*----------  Datos del servidor  ----------*/
+	define("SERVER", $_ENV["DB_HOST"] ?? "localhost");
+	define("DB", $_ENV["DB_DATABASE"] ?? "ser_mecanica");
+	define("USER", $_ENV["DB_USERNAME"] ?? "root");
+	define("PASS", $_ENV["DB_PASSWORD"] ?? "");
 
 	define("SGBD", "mysql:host=".SERVER.";dbname=".DB);
 
 	/*----------  Datos de la encriptación  ----------*/
-	define("METHOD", getenv("METHOD") ?: "AES-256-CBC");
-	define("SECRET_KEY", getenv("SECRET_KEY") ?: '$STO@2021');
-	define("SECRET_IV", getenv("SECRET_IV") ?: '102791');
+	define("METHOD", $_ENV["METHOD"] ?? "AES-256-CBC");
+	define("SECRET_KEY", $_ENV["SECRET_KEY"] ?? '$STO@2021');
+	define("SECRET_IV", $_ENV["SECRET_IV"] ?? '102791');
 
